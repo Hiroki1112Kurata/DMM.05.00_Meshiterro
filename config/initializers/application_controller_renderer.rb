644c47b268fbb229625d1d64cff2dae70a70e5c5
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-06-16 08:57:21 +0000

    [Add]5-4_favorite

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '57532bbb9ad43c7e6286eea0fe18891630ad1986c0990bf078c4e6a426cbabba061329fdd070c6c1182d91f9f27f64fc98a44362d1b623d37fe3d63fda563dab'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    # 単数にすると、idがリクエストに含まれなくなる
+    resource :favorites, only: [:create, :destroy]
+
+    resources :post_comments, only: [:create, :destroy]
+  end
 
 end
